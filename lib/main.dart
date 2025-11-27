a07702915e15e36f275fbe198a7e4fda84a44af1
MusicLuminous <music-yinlinghan@outlook.com>
2025-11-27 23:47:49 +0800

    3

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Luminous_minecraft_launcher',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: MyHomePage(),
     );
